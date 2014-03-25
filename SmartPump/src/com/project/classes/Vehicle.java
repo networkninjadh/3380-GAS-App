5fe634e161a63ca6bd61f5eff6ceb8ea90dd054e
networkninjadh <networkninjadh@gmail.com>
2014-03-25 02:06:48 -0500

    add javadoc

--- SmartPump/src/com/project/classes/Vehicle.java
+++ SmartPump/src/com/project/classes/Vehicle.java
@@ -10,28 +10,57 @@ public class Vehicle
 	private String vehicleModel;
 	private String vehicleID;
 	private int vehicleYear;
+	private String vehicleProfileName; //ID to keep track of vehicle profile
 	
-	Vehicle(int vehicleYear, String vehicleMake, String vehicleModel, String vehicleID)
+	/**
+	 * 
+	 * @param vehicleYear
+	 * @param vehicleMake
+	 * @param vehicleModel
+	 * @param vehicleID
+	 * @param vehicleProfileName
+	 */
+	Vehicle(int vehicleYear, String vehicleMake, String vehicleModel, String vehicleID, String vehicleProfileName)
 	{
 		this.vehicleID = vehicleID;
 		this.vehicleMake = vehicleMake;
 		this.vehicleModel = vehicleModel;
 		this.vehicleYear = vehicleYear;
+		this.vehicleProfileName = vehicleProfileName;
 	}
+	/**
+	 * @return int
+	 */
 	int getVehicleYear()
 	{
 		return this.vehicleYear;
 	}
+	/**
+	 * @return String
+	 */
 	String getVehicleMake()
 	{
 		return this.vehicleMake;
 	}
+	/**
+	 * @return String
+	 */
 	String getVehicleModel()
 	{
 		return this.vehicleModel;
 	}
+	/**
+	 * @return String
+	 */
 	String getVehicleID()
 	{
 		return this.vehicleID;
+	}
+	/**
+	 * @return String
+	 */
+	String getVehicleProfileName()
+	{
+		return this.vehicleProfileName;
 	}
 }
