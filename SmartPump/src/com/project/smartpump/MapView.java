39c0a97c8a90a3853fce845e09f4c63ba5492044
networkninjadh <networkninjadh@gmail.com>
2014-03-28 18:25:45 -0500

    add updates

--- SmartPump/src/com/project/smartpump/MapView.java
+++ SmartPump/src/com/project/smartpump/MapView.java
@@ -7,18 +7,19 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 public class MapView extends FragmentActivity
-{	private GoogleMap gMap;
-	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{	super.onCreate(savedInstanceState);
-		setContentView(R.layout.map);
-        gMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+{       private GoogleMap gMap;
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+        {       super.onCreate(savedInstanceState);
+                setContentView(R.layout.map);
+     
+                gMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
         LatLng sydney = new LatLng(-33.867, 151.206);
         gMap.setMyLocationEnabled(true);
         gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney,13));
         gMap.addMarker(new MarkerOptions()
-        	.title("Sydney")
-        	.snippet("The most populous city in Austrailia.")
-        	.position(sydney));
-	}
+                .title("Sydney")
+                .snippet("The most populous city in Austrailia.")
+                .position(sydney));
+        }
 }
